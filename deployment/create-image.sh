58cbe92ad7e46c307982ad430b13843fc1ab7973
tfm <jcdiaztorres96@gmail.com>
2020-06-09 03:08:30 -0700

    New deployment files info

--- deployment/create-image.sh
+++ deployment/create-image.sh
@@ -1,2 +1,3 @@
-#openstack image create --disk-format qcow2 --container-format bare --public --file ubuntu-16.04-server-cloudimg-amd64-disk1.img ubuntu-16
+source /etc/kolla/admin-openrc.sh
+openstack image create --disk-format qcow2 --container-format bare --public --file ubuntu-16.04-server-cloudimg-amd64-disk1.img ubuntu-16
 openstack image create --disk-format qcow2 --container-format bare --public --file ubuntu-18.04-server-cloudimg-amd64.img ubuntu-18
--- deployment/create-vm.sh
+++ deployment/create-vm.sh
@@ -1,1 +1,3 @@
-openstack server create --flavor m1.medium --image ubuntu-18 --key-name  mykey --network demo-net --user-data=user-data.txt ubuntu-18
+source /etc/kolla/admin-openrc.sh
+openstack server create --flavor t1.sce --image ubuntu-18 --key-name  mykey --network demo-net --user-data user-data.txt ubuntu-18
+#openstack server create --flavor t1.sce --image ubuntu-16 --key-name  mykey --network demo-net --user-data user-data.txt ubuntu-16
